f1ed80cabc696f30d2c755ebd51c35364a6e96ab
RadixGG <radixholms@gmail.com>
2020-03-25 04:09:39 +0800

    update binary search tree

--- ds/bstree.go
+++ ds/bstree.go
@@ -12,8 +12,8 @@ type BSTree struct {
 type BSTNode struct {
 	Key int
 	Value *[]interface{}
-	LeftChild *BSTNode
-	RightChild *BSTNode
+	Left *BSTNode
+	Right *BSTNode
 }
 
 func (t *BSTree) Len() uint {
@@ -21,77 +21,142 @@ func (t *BSTree) Len() uint {
 }
 
 func (t *BSTree) Append(key int, val interface{}) *BSTree {
-	curNode := t.root
-	if curNode == nil {
-		t.root = &BSTNode{
-			Key: key,
-		}
-		if val != nil {
-			tmp := make([]interface{}, 1)
-			tmp[0] = val
-			t.root.Value = &tmp
-		}
-		return t
-	}
-	for curNode != nil {
-		if key < curNode.Key {
-			if curNode.LeftChild == nil {
-				curNode.LeftChild = &BSTNode{
-					Key: key,
-				}
-				if val != nil {
-					tmp := make([]interface{}, 1)
-					tmp[0] = val
-					curNode.LeftChild.Value = &tmp
-				}
-				break
-			} else {
-				curNode = curNode.LeftChild
+	var appendNode func(int, interface{}, *BSTNode) *BSTNode
+	appendNode = func(key int, val interface{}, node *BSTNode) *BSTNode {
+		if node == nil {
+			node = &BSTNode{
+				Key: key,
 			}
-		} else if key > curNode.Key {
-			if curNode.RightChild == nil {
-				curNode.RightChild = &BSTNode{
-					Key: key,
-				}
-				if val != nil {
-					tmp := make([]interface{}, 1)
-					tmp[0] = val
-					curNode.RightChild.Value = &tmp
-				}
-				break
-			} else {
-				curNode = curNode.RightChild
-			}
-		} else {
+			t.count++
 			if val != nil {
-				if curNode.Value != nil {
-					*curNode.Value = append(*curNode.Value, val)
-				} else {
-					tmp := make([]interface{}, 1)
-					tmp[0] = val
-					curNode.Value = &tmp
-				}
+				tmp := make([]interface{}, 1)
+				tmp[0] = val
+				node.Value = &tmp
 			}
-			break
+			return node
+		} else if key == node.Key {
+			if node.Value != nil {
+				*node.Value = append(*node.Value, val)
+			} else {
+				tmp := make([]interface{}, 1)
+				tmp[0] = val
+				node.Value = &tmp
+			}
+			return node
 		}
+
+		if key < node.Key {
+			node.Left = appendNode(key, val, node.Left)
+		} else {
+			node.Right = appendNode(key, val, node.Right)
+		}
+
+		return node
 	}
+	t.root = appendNode(key, val, t.root)
 	return t
 }
 
 func (t *BSTree) Search(key int) (*BSTNode, error) {
-	curNode := t.root
-	for curNode != nil {
+	current := t.root
+	for current != nil {
 		switch {
-		case key < curNode.Key:
-			curNode = curNode.LeftChild
-		case key > curNode.Key:
-			curNode = curNode.RightChild
+		case key < current.Key:
+			current = current.Left
+		case key > current.Key:
+			current = current.Right
 		default:
-			return curNode, nil
+			return current, nil
 		}
 	}
 	return nil, errors.New("not found")
 }
 
-//func (t *BSTree) Remove(key int) *BSTree {
-//}
+func (t *BSTree) Remove(key int) *BSTree {
+	// 移除节点定位
+	var findPNode func(int, *BSTNode) (*BSTNode, int)
+	findPNode = func(key int, node *BSTNode) (*BSTNode, int) {
+		if key == node.Key {
+			return nil, 0
+		} else if node == nil {
+			return nil, -1
+		}
+		var record int // 1代表要移除的节点为返回节点的左子节点，2反之，-1代表没找到
+		if key < node.Key {
+			tmp, tmp2 := findPNode(key, node.Left)
+			if tmp != nil {
+				node = tmp
+			}
+			if tmp2 == 0 {
+				record = 1
+			} else {
+				record = tmp2
+			}
+		} else if key > node.Key {
+			tmp, tmp2 := findPNode(key, node.Right)
+			if tmp != nil {
+				node = tmp
+			}
+			if tmp2 == 0 {
+				record = 2
+			} else {
+				record = tmp2
+			}
+		}
+		return node, record
+	}
+
+	// 取出替代节点，即取出移除节点右子树中的最小值
+	var takeMin func(pNode *BSTNode, node *BSTNode, flag bool) *BSTNode
+	takeMin = func(pNode *BSTNode, node *BSTNode, flag bool) *BSTNode {
+		if node == nil {
+			return node
+		} else if node.Left == nil {
+			if flag { // 函数调用时，flag恒传true，即初次为true
+				pNode.Right = node.Right
+			} else {
+				pNode.Left = node.Right
+			}
+			return node
+		}
+		node = takeMin(node, node.Left, false)
+		return node
+	}
+
+	// 移除节点
+	node, record := findPNode(key, t.root)
+	if record == -1 {
+		return t
+	} else if record == 0 {
+		takeMinNode := takeMin(t.root, t.root.Right, true)
+		if takeMinNode == nil {
+			t.root = t.root.Left
+		} else {
+			takeMinNode.Left = t.root.Left
+			takeMinNode.Right = t.root.Right
+			t.root = takeMinNode
+		}
+	} else if record == 1 {
+		takeMinNode := takeMin(node.Left, node.Left.Right, true)
+		if takeMinNode == nil {
+			node.Left = node.Left.Left
+		} else {
+			takeMinNode.Left = node.Left.Left
+			takeMinNode.Right = node.Left.Right
+			node.Left = takeMinNode
+		}
+	} else if record == 2 {
+		takeMinNode := takeMin(node.Right, node.Right.Right, true)
+		if takeMinNode == nil {
+			node.Right = node.Right.Left
+		} else {
+			takeMinNode.Left = node.Right.Left
+			takeMinNode.Right = node.Right.Right
+			node.Right = takeMinNode
+		}
+	} else {
+		panic("unknown")
+	}
+
+	return t
+}
--- ds/ds_test.go
+++ ds/ds_test.go
@@ -1,17 +1,12 @@
 package ds_test
 
 import (
-	"fmt"
-	"radix/ds"
+	"GoRadix/ds"
 	"testing"
 )
 
 func TestBSTree(t *testing.T) {
-	var d *[]interface{}
-	tmp := make([]interface{}, 1)
-	d = &tmp
-	*d = append(*d, 123)
-	fmt.Println(*d)
+
 }
 
 func TestLList(t *testing.T) {
@@ -20,29 +15,29 @@ func TestLList(t *testing.T) {
 	d.Append(1)
 	d.Insert(2, 0)
 	d.Insert(3, 1)
-	n1, err := d.LLNode(0)
+	n1, err := d.Search(0)
 	if n1.Value.(int) != 2 || err != nil {
 		t.Fatal("此元素应该为2")
 	}
-	n2, err := d.LLNode(1)
+	n2, err := d.Search(1)
 	if n2.Value.(int) != 3 || err != nil {
 		t.Fatal("此元素应该为3")
 	}
-	n3, err := d.LLNode(2)
+	n3, err := d.Search(2)
 	if n3.Value.(int) != 0 || err != nil {
 		t.Fatal("此元素应该为0")
 	}
-	n4, err := d.LLNode(3)
+	n4, err := d.Search(3)
 	if n4.Value.(int) != 1 || err != nil {
 		t.Fatal("此元素应该为1")
 	}
 	d.Remove(0)
 	d.Remove(2)
-	n5, err := d.LLNode(0)
+	n5, err := d.Search(0)
 	if n5.Value.(int) != 3 || err != nil {
 		t.Fatal("此元素应该为3")
 	}
-	n6, err := d.LLNode(1)
+	n6, err := d.Search(1)
 	if n6.Value.(int) != 0 || err != nil {
 		t.Fatal("此元素应该为0")
 	}
@@ -61,29 +56,29 @@ func TestDLList(t *testing.T) {
 	d.Append(1)
 	d.Insert(2, 0)
 	d.Insert(3, -2)
-	n1, err := d.DLLNode(0)
+	n1, err := d.Search(0)
 	if n1.Value.(int) != 2 || err != nil {
 		t.Fatal("此元素应该为2")
 	}
-	n2, err := d.DLLNode(1)
+	n2, err := d.Search(1)
 	if n2.Value.(int) != 3 || err != nil {
 		t.Fatal("此元素应该为3")
 	}
-	n3, err := d.DLLNode(-2)
+	n3, err := d.Search(-2)
 	if n3.Value.(int) != 0 || err != nil {
 		t.Fatal("此元素应该为0")
 	}
-	n4, err := d.DLLNode(-1)
+	n4, err := d.Search(-1)
 	if n4.Value.(int) != 1 || err != nil {
 		t.Fatal("此元素应该为1")
 	}
 	d.Remove(0)
 	d.Remove(-1)
-	n5, err := d.DLLNode(0)
+	n5, err := d.Search(0)
 	if n5.Value.(int) != 3 || err != nil {
 		t.Fatal("此元素应该为3")
 	}
-	n6, err := d.DLLNode(-1)
+	n6, err := d.Search(-1)
 	if n6.Value.(int) != 0 || err != nil {
 		t.Fatal("此元素应该为0")
 	}
